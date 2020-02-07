c6e22d97728988b358c49e3bcc6418e3eca5d2f8
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-07 07:14:20 -0600

    in progress

--- src/yasmin/Kata.java
+++ src/yasmin/Kata.java
@@ -1,5 +1,23 @@
 package yasmin;
 
 public class Kata {
+    public static void main(String[] args) {
+        System.out.println(remove("yasmin"));
+}
 
+
+    // TODO Count Odd Numbers below n.
+    //  Given a number n, return the number of positive odd numbers below n, EASY!
+
+    public static int oddCount(int n) {
+        return n / 2;
+    }
+
+
+    // TODO Remove First and Last Character
+
+    public static String remove(String str) {
+        //substring(int beginIndex, int endIndex)
+        return str.substring(1, str.length() - 1);
+    }
 }
--- src/yasmin/YasminTest.java
+++ src/yasmin/YasminTest.java
@@ -1,9 +1,26 @@
 package yasmin;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class YasminTest {
-    public static void main(String[] args) {
-        Kata test = new Kata();
+    public static int rand(int a, int b) {
+        int x = (int) (Math.random() * ((b - a) + 1)) + a;
+        return x;
+    }
 
+    @Test
+    public void fixedTests() {
+        assertEquals(7, Kata.oddCount(15));
+        assertEquals(7511, Kata.oddCount(15023));
+    }
 
+    @Test
+    public void randomTest() {
+        for (int i = 10; i < 1000; i++) {
+            int x = rand((int) (2e9), 2147483647);
+            assertEquals(x / 2, Kata.oddCount(x));
+        }
     }
 }
