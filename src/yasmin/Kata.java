0cc76f34c45ce688e7bdedcedfc9714f9190b6c3
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-07 07:35:12 -0600

    lab stand ups

--- src/yasmin/Kata.java
+++ src/yasmin/Kata.java
@@ -1,9 +1,12 @@
 package yasmin;
 
+import java.util.Scanner;
+
 public class Kata {
     public static void main(String[] args) {
         System.out.println(remove("yasmin"));
-}
+        System.out.println(oddCount(8));
+    }
 
 
     // TODO Count Odd Numbers below n.
@@ -19,5 +22,22 @@ public class Kata {
     public static String remove(String str) {
         //substring(int beginIndex, int endIndex)
         return str.substring(1, str.length() - 1);
+    }
+}
+
+
+// TODO Check a Leap Year
+class LeapYear {
+    public static void main(String[] args) {
+        int inputYear;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any Year:");
+        inputYear = scan.nextInt();
+
+        // evenly divisible by 4 and 100 or 400
+        if (((inputYear % 4 == 0) && (inputYear % 100 != 0)) || (inputYear % 400 == 0))
+            System.out.format("%s year is a leap year", inputYear);
+        else
+            System.out.format("%s year is a not a leap year", inputYear);
     }
 }
