802cb2802f58aff2ce03e985a58f2d46c9b2e41b
Hung Ly <hunghly@gmail.com>
2020-02-07 15:18:17 -0600

    did some random challanges

--- src/HungExercises.java
+++ src/HungExercises.java
@@ -13,11 +13,31 @@ public class HungExercises {
 //        factors(50);
 //        factors(86240);
 //        jumpingNumber(556847);
-        System.out.println(sumDigPow(2627876, 2647441));
+//        System.out.println(sumDigPow(2627876, 2647441));
         /*2627876
 2647441*/
+//        System.out.println(tidyNumber(212));
     }
 
+    /*Definition
+    A Tidy number is a number whose digits are in non-decreasing order.
+    Given a number, Find if it is Tidy or not .
+    tidyNumber (12) ==> return (true)
+    */
+    public static boolean tidyNumber(int number) {
+        String[] numberArray = Integer.toString(number).split("");
+        for (int i = 0; i < numberArray.length; i++) {
+            if (i == numberArray.length - 1) break;
+            if (Integer.parseInt(numberArray[i]) > Integer.parseInt(numberArray[i+1])) return false;
+        }
+        return true;
+    }
+
+    /*The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
+    In effect: 89 = 8^1 + 9^2
+    The next number in having this property is 135.
+    See this property again: 135 = 1^1 + 3^2 + 5^3
+    We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.*/
     public static List<Long> sumDigPow(long a, long b) {
         long sum;
         List<Long> returnList = new ArrayList<>();
