323c1890fed6e8d96845ec718327ad47d9e0814f
Dayna Leal <dayna.r.leal@gmail.com>
2020-02-09 21:58:18 -0600

    added notes

--- src/daynaLeal/edabit2.java
+++ src/daynaLeal/edabit2.java
@@ -4,13 +4,15 @@ public class edabit2 {
     public static void main(String[] args) {
         int[] test1 = {10, 4, 1, 4, -10, -50, 32, 21};
         int[] test2 = {44, 32, 86, 19};
-//        System.out.println(differenceMaxMin(test1)); //82
+        System.out.println(differenceMaxMin(test1)); //82 examplest taken from edabit
         System.out.println(differenceMaxMin(test2)); //67
 
     }
 
 
-
+    //1
+    //Difference of Max and Min Numbers in Array
+    //Create a function that takes an array and returns the difference between the biggest and smallest numbers.
     public static int differenceMaxMin(int[] arr) {
         int max = 0;
         int min = 0;
