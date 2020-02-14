d46155033b0d3ec00c8d66ca39c4df9f04aa2664
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-14 10:54:16 -0600

    lab stand ups

--- src/yasmin/Kata.java
+++ src/yasmin/Kata.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class Kata {
     public static void main(String[] args) {
+
         System.out.println(remove("yasmin"));
+
         System.out.println(oddCount(8));
+
+        System.out.println(zeroFuel(50, 25, 2));// true
+        System.out.println(zeroFuel(100, 50, 1)); // false
     }
 
 
@@ -23,8 +28,13 @@ public class Kata {
         //substring(int beginIndex, int endIndex)
         return str.substring(1, str.length() - 1);
     }
-}
 
+    //TODO You were camping with your friends far away from home, but when it's time to go back, you realize that you fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true if it is possible and false if not. The input values are always positive.
+
+    static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+        return distanceToPump <= mpg * fuelLeft;
+    }
+}
 
 // TODO Check a Leap Year
 class LeapYear {
